42866dfb8fafcb975a9cb18c60d4eb20147f8515
indotek68 <angga.w.putra@gmail.com>
2014-08-31 16:42:09 -0700

    playing around with charts

--- bodybudget/app/assets/javascripts/users.js
+++ bodybudget/app/assets/javascripts/users.js
@@ -1,8 +1,11 @@
+
+
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).ready(function(){
 
-var data = [
+  var data = [
     {
         value: 30,
         color:"#F7464A",
@@ -10,7 +13,7 @@ var data = [
         label: "Red"
     },
     {
-        value: 30,	
+        value: 30,  
         color: "#46BFBD",
         highlight: "#5AD3D1",
         label: "Green"
@@ -24,24 +27,32 @@ var data = [
     }
 ];
 
-window.onload = function(){
-	var ctx = $('#chart-area').get(0).getContext("2d");
-	window.myDoughnut = new Chart(ctx).Doughnut(data, {
-		responsive : true,
-		animateRotate: true,
-		animateScale: false,
-		percentageInnerCutout: 70,
-		animationEasing : "easeInOutQuart"
+
+	var ctx = $('#chart-area')[0].getContext("2d");
+	var myDoughnut = new Chart(ctx).Doughnut(data, {
+			responsive : true,
+			animateRotate: true,
+			animateScale: false,
+			percentageInnerCutout: 70,
+			animationEasing : "easeInOutQuart"
 	});
 
-		$(".values p").hover(
-	  function() {
-	    $(this).append( $( "<span> ***</span>" ) );
-	  }, function() {
-	    $( this ).find( "span:last" ).remove();
-	  }
-	);
-};
+  function updateData() {
+      data.forEach(function(entry) {
+          entry.value += 10;
+      });
+  }
+
+  document.getElementById("button").onclick = function() {
+	updateData(); // for demo only
+	var index = 0;
+
+    myDoughnut.segments[0].forEach(function(segment) {
+      segment.value = data[index++].value;
+    });
+      myDoughnut.update();
+  };
+});
 
 
 
