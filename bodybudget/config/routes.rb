29be11da077528f779b30080fdf67fca7cb26f39
indotek68 <angga.w.putra@gmail.com>
2014-08-31 10:34:13 -0700

    added chart example and hover over test

--- bodybudget/config/routes.rb
+++ bodybudget/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'users#index'
+
+  resources :users 
+
   get   '/login', :to => 'sessions#new', :as => :login
   get '/auth/:provider/callback', :to => 'sessions#create'
   get '/auth/failure', :to => 'sessions#failure'
