4c66ab1b856a08ce8111954f1b96d8818367ade3
indotek68 <angga.w.putra@gmail.com>
2014-09-01 12:03:23 -0700

    did some testing with merging and added bootstrap to vendors/assets

--- bodybudget/app/assets/javascripts/users.js
+++ bodybudget/app/assets/javascripts/users.js
@@ -27,10 +27,40 @@ $(document).ready(function() {
 
 //refactoring search api code
 //search yummly api
+var foodIdArray = [];
+var course = "";
+var date = new Date();
+var time = date.getHours();
+
+
+if (time >= 5 && time < 12){
+	course = "Breakfast and Brunch";
+	$('.course-time').append("<h2> It's "+course+" time</h2>");
+} else if (time >= 12 && time < 16){
+	course = "Lunch";
+	$('.course-time').append("<h2> It's "+course+" time</h2>");
+} else if (time >=16 && time < 24) {
+	course = "Main Dishes";
+	('.course-time').append("<h2> It's "+course+" time</h2>");
+} else {
+	$('.course-time').append("<h2> You should be asleep </h2>");
+}
+
 function searchFood(search){
 	return $.ajax({ 
 		url: "http://api.yummly.com/v1/api/recipes?_app_id=25e7217b&_app_key=26571850ea53d59ce432e9b9448f16b9&q="+search,
 		data: {
+			'maxResult': 10,
+			"rating": 5,
+			"allowedCourse[]": "course^course-"+course,
+			// "flavor.meaty.min": 0,
+			// "flavor.meaty.max": 0,
+			// "flavor.sweet.min": 0,
+			// "flavor.sweet.max": 0,
+			// "flavor.sweet.min": 0,
+			// "flavor.sweet.max": 0,
+			// "flavor.piquant.min": 0,
+			// "flavor.piquant.max": 0,
 			"requirePictures": "true",
 			format: "json"
 		},
@@ -38,20 +68,26 @@ function searchFood(search){
 	});
 }
 
+// function searchFoodId(search){
+// 	return $.getJSON(//api.yummly.com/v1/api/recipe/recipe-id?_app_id=YOUR_ID&_app_key=YOUR_APP_KEY
+// }
+
 	$(".getInfo").on('submit', function(e){
 		e.preventDefault();
 		$(".user-data").html("");
 		var recipe = $('.recipeName').val();
-			//defered promise
 
+		//defered promise
 		$.when(searchFood(recipe)).done(function(result){
 			$('.user-data').html("");
-			
+			console.log(result)
 			if(result.matches.length === 0){
 				$('.user-data').append("<h2>Sorry nothing came up!</h2>");
 			} else {
 				result.matches.forEach(function (itm){
-					$(".user-data").append("<p>"+itm.recipeName+"</p>")
+
+					// $(".user-data").append("<img src="itm. "/>")
+					$(".user-data").append("<p>"+itm.id+"</p>")
 				})
 			}
 		});
