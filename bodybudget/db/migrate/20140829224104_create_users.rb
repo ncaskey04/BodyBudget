eaa0731581b135e3b241c4d022969b8fe7b53bdc
ncaskey04 <ncaskey04@gmail.com>
2014-09-02 15:03:51 -0700

    oauth complete

--- bodybudget/db/migrate/20140829224104_create_users.rb
+++ bodybudget/db/migrate/20140829224104_create_users.rb
@@ -3,9 +3,16 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :provider
       t.string :uid
-      t.string :first_name
-      t.string :last_name
-      t.string :email
+      t.string :full_name
+      t.string :gender
+      t.string :about_me
+      t.string :city
+      t.string :state
+      t.string :country
+      t.date :dob
+      t.date :member_since
+      t.string :locale
+      t.string :timezone
       t.timestamps
     end
   end
