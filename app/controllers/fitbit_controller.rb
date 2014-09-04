ee39aa3a4fb000669fb51c45c3bba5d75d2fe1ce
ncaskey04 <ncaskey04@gmail.com>
2014-09-04 09:41:13 -0700

    updated static date in dynamic and removed fitbit stuff

--- app/controllers/fitbit_controller.rb
+++ app/controllers/fitbit_controller.rb
@@ -8,18 +8,15 @@ class FitbitController < ApplicationController
 
   def show
 
+    # u.fitbit.data_by_time_range("/activities/tracker/steps", {:base_date => DateTime.now.strftime('%Y-%m-%d'), :period => "1d"})
+
     @current_user = User.find(session[:user_id])
 
      gon.results_food = @current_user.fitbit.recent_foods
-     gon.calsIn = @current_user.fitbit.data_by_time_range("/foods/log/caloriesIn",{:base_date => "2014-09-03",:period => "1d"})
-     gon.calsOut = @current_user.fitbit.data_by_time_range("/activities/tracker/calories",{:base_date => "2014-09-03",:period => "1d"})
-     gon.miles = @current_user.fitbit.data_by_time_range("/activities/tracker/distance",{:base_date => "2014-09-03",:period => "1d"})
-     gon.steps = @current_user.fitbit.data_by_time_range("/activities/tracker/steps",{:base_date => "2014-09-03",:period => "1d"})
-    
-    puts @results_calsIn
-    puts @results_calsOut
-    puts @results_miles
-    puts @results_steps
+     gon.calsIn = @current_user.fitbit.data_by_time_range("/foods/log/caloriesIn", {:base_date => DateTime.now.strftime('%Y-%m-%d'), :period => "1d"})
+     gon.calsOut = @current_user.fitbit.data_by_time_range("/activities/tracker/calories", {:base_date => DateTime.now.strftime('%Y-%m-%d'), :period => "1d"})
+     gon.miles = @current_user.fitbit.data_by_time_range("/activities/tracker/distance", {:base_date => DateTime.now.strftime('%Y-%m-%d'), :period => "1d"})
+     gon.steps = @current_user.fitbit.data_by_time_range("/activities/tracker/steps", {:base_date => DateTime.now.strftime('%Y-%m-%d'), :period => "1d"})
 
   end
 
